3a8dc10e7dddacd73abcfc5f03fdd34825f66360
Yuchen Peng <yuch.pp@gmail.com>
2021-10-08 12:40:57 -0400

    update ticker.py get historical stock price

--- common/ticker.py
+++ common/ticker.py
@@ -1,16 +1,28 @@
 import requests
 
 
-def _parse_yahoo_data(quote_data):
-    yahoo_data = quote_data.get('quoteResponse', {}).get('result', [{}])
-    try:
-        symbol, prevClose = yahoo_data['symbol'], yahoo_data['regularMarketPreviousClose']
-        return symbol, prevClose
-    except:
-        return None, None
+def _parse_yahoo_data(quote_data, type='last'):
+    if type == 'last':
+        yahoo_data = quote_data.get('quoteResponse', {}).get('result', [{}])
+        try:
+            symbol, prevClose = yahoo_data['symbol'], yahoo_data['regularMarketPreviousClose']
+            return symbol, None, prevClose
+        except:
+            return None, None, None
+    elif type == 'history':
+        try:
+            yahoo_data = quote_data.get('chart', {}).get('result', [{}])[0]
+            symbol = yahoo_data['meta']['symbol']
+            timestamp = yahoo_data['timestamp']
+            adjclose = yahoo_data['indicators']['adjclose'][0]['adjclose']
+            return symbol, timestamp, adjclose
+        except:
+            return None, None, None
+    else:
+        raise ValueError('Unknown type!')
 
 
-def get_ticker_price(ticker):
+def get_prev_close(ticker):
     url = "https://query1.finance.yahoo.com/v7/finance/quote?lang=en-US&region=US&corsDomain=finance.yahoo.com"
     r = requests.get(
         url,
@@ -19,4 +31,20 @@ def get_ticker_price(ticker):
             'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
         }
     )
-    return _parse_yahoo_data(r.json())
+    return _parse_yahoo_data(r.json(), type='last')
+
+
+def get_price_history(ticker, interval='1d', range='6mo'):
+    url = "https://query1.finance.yahoo.com/v8/finance/chart/AAPL"
+    r = requests.get(
+        url,
+        params={
+            'interval': interval,
+            'range': range,
+            'events': "div,splits"
+            },
+        headers={
+            'user-agent': "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36"
+        }
+        )
+    return _parse_yahoo_data(r.json(), type='history')
